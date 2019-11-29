03b00d3f3e9bb5ef0badf22611f2a0adefa4afac
Luke Perry <ldperry93@gmail.com>
2019-11-29 14:02:25 -0500

    update send email interaction

--- JS/Main.js
+++ JS/Main.js
@@ -4,4 +4,28 @@ $(document).ready(function() {
             scrollTop: $("#begin_main ").offset().top
         }, 1000)
     });
+
+    // Email
+    $( "#contact_me" ).submit(function( event ) {
+        event.preventDefault();
+        var $inputs = $('#contact_me :input');
+
+        var values = {};
+        $inputs.each(function() {
+            values[this.name] = $(this).val();
+        });
+        
+        if (!values.name || values.name.length < 1 || !values.email || values.email.length < 1 || !values.message || values.message.length < 1) {
+            if ($('#email-message').length === 0) {
+                $('#form-container').append('<div id="email-message">Please fill out the whole form</div>'); 
+            }
+            return
+        }
+        
+        if ($('#email-message').length !== 0) {
+            $('#email-message').empty()
+        }
+        $('#contact_me').slideUp();
+        $('#form-container').append('<div id="email-message">Thanks for reaching out! I will be in touch soon!</div>'); 
+    });    
 });
